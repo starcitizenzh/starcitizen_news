9df8e490ce4c706f0c4e223cd00c2675061e4942
starcitizenzh <99375527+starcitizenzh@users.noreply.github.com>
2025-09-27 10:43:19 +0700

    Create add_background.js

---
+++ plugins/add_background.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+      const imageUrl = "https://wallup.net/wp-content/uploads/2019/09/167652-star-citizen-sci-fi-spaceship-game-space.png";
+      document.body.style.backgroundImage = `url(${imageUrl})`;
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.margin = "0";
+      document.body.style.height = "100vh";
+    };
